a049e5da886798ddd948fe73708d45bcdf2b8628
Michelle Ranly <meranly8@gmail.com>
2020-01-08 17:25:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,30 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  next = [...document.body.querySelector(`)]
+  next = [...document.body.querySelector('div#grand-node').children]
+  console.log(`next = ${next}`)
+  depth = 0
+  current = next.shift()
+  while (current !== undefined) {
+    try {
+      while (current.child !== undefined) {
+        if(current.child.length > 0) {
+          for (let i = 0; i < current.child.length; i++) {
+            next.push(current.child[i])
+            console.log(`current = ${current}`)
+          }
+        } else { 
+          console.log(`There's no children here.`)
+          console.log(current)
+          return(current)
+      }
+    }
+    catch(error){
+      console.log(error)
+    }
+    return current
+  }
 }
+
+deepestChild()
+
